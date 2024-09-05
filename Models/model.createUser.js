11a0d3814ce17732d70790fce601cd1425ec0126
devaziz1 <157882617+devaziz1@users.noreply.github.com>
2024-09-05 21:27:30 +0500

    owais APIs added

---
+++ Models/model.createUser.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const schema = mongoose.Schema;
+
+// CreateUser Schema
+const createUserSchema = new schema({
+    fullName: {
+        type: String,
+        required: true
+    },
+    contact: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    aboutYou: {
+        type: String
+    },
+
+    date_time: {
+        type: Date,
+        default: Date.now
+    },
+});
+
+// Create the model
+const CreateUser = mongoose.model('CreateUser', createUserSchema);
+
+// Export the model
+module.exports = CreateUser;
